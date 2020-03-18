6a3611e32e3bc78c2ea75f2e730b5cefd23eef24
Russ Smith <gumper@gumper.com>
2020-03-18 05:24:19 -0700

    More explicit syntax - used @@global

--- super_read_only.rb
+++ super_read_only.rb
@@ -6,7 +6,7 @@ begin
 
     client = Mysql2::Client.new(host: "127.0.0.1", port: "13306", username: ENV["MYSQL_USERNAME"], password: ENV["MYSQL_PASSWORD"])
 
-    client.query("set global read_only=ON, super_read_only=ON")
+    client.query("set @@global.read_only=ON, @@global.super_read_only=ON")
 
 rescue Mysql2::error => e
     puts e.errno
