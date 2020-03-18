908d3fb5d664813a5df1f1efec499e181d696e6c
Russ Smith <gumper@gumper.com>
2020-03-18 05:16:34 -0700

    Script for setting multiple variables at once

---
+++ super_read_only.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+
+require 'mysql2'
+
+begin
+
+    client = Mysql2::Client.new(host: "127.0.0.1", port: "13306", username: ENV["MYSQL_USERNAME"], password: ENV["MYSQL_PASSWORD"])
+
+    client.query("set global read_only=1, super_read_only=1")
+
+rescue Mysql2::error => e
+    puts e.errno
+    puts e.error
+
+ensure
+    client.close if client
+
+end
+
