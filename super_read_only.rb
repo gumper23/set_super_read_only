bc99acd84a6d1f7832d3f292774a8c6bf5e26abb
Russ Smith <gumper@gumper.com>
2020-03-18 05:21:27 -0700

    Changed from 1 to ON

--- super_read_only.rb
+++ super_read_only.rb
@@ -6,7 +6,7 @@ begin
 
     client = Mysql2::Client.new(host: "127.0.0.1", port: "13306", username: ENV["MYSQL_USERNAME"], password: ENV["MYSQL_PASSWORD"])
 
-    client.query("set global read_only=1, super_read_only=1")
+    client.query("set global read_only=ON, super_read_only=ON")
 
 rescue Mysql2::error => e
     puts e.errno
